d3a003cac5c342c6b6620ad572be0ffd05858027
Gabriel Alabi <Rexes_Games@yahoo.com>
2023-06-30 12:53:07 +0100

    attempt to extract samples

--- Unity Soundfont Player/Assets/SoundFont Player/Scripts/Editor/SoundFontPlayerEditor.cs
+++ Unity Soundfont Player/Assets/SoundFont Player/Scripts/Editor/SoundFontPlayerEditor.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using NAudio.SoundFont;
+using NAudio.Wave;
 using UnityEditor;
 using UnityEngine;
 
@@ -74,6 +77,56 @@ public class SoundFontPlayerEditor : Editor
         if (GUILayout.Button("Stop Instrument"))
         { 
             soundFontPlayer.StopInstrument();  
+        }
+    }
+
+    public void ExtractSamples(in SoundFontPlayer soundFontPlayer)
+    {
+        if (soundFontPlayer.Soundfont == null) return;
+        
+        //Create Extracted Soundfonts Directory
+        string extractedSoundFontsPath = Application.dataPath + "/SoundFont Player" + "/Extracted SoundFonts";
+        
+        if(!Directory.Exists(extractedSoundFontsPath))
+        {
+            Directory.CreateDirectory(extractedSoundFontsPath);
+        }
+        
+        // Create the output folder if it doesn't exist
+        string outputFolder = extractedSoundFontsPath + soundFontPlayer.Soundfont.FileInfo.BankName;
+        
+        if(!Directory.Exists(extractedSoundFontsPath))
+        {
+            Directory.CreateDirectory(outputFolder);
+        }
+        
+
+        // Iterate over each instrument in the SoundFont
+        foreach (var instrument in soundFontPlayer.Soundfont.Instruments)
+        {
+            // Iterate over each sample in the instrument
+            foreach (var zone in instrument.Zones)
+            {
+                foreach (var generator in zone.Generators)
+                {
+                    if (generator.GeneratorType == GeneratorEnum.SampleID)
+                    {
+                        var sampleIndex = generator.UInt16Amount;
+                        var sample = soundFontPlayer.Soundfont.SampleHeaders[sampleIndex];
+
+                        // Extract sample data as PCM audio
+                        var audioData = soundFontPlayer.Soundfont.SampleData[sampleIndex];
+
+                        // Create a WAV file path
+                        var wavFilePath = Path.Combine(outputFolder, $"{instrument.Name}_{sample.SampleName}.wav");
+
+                        using(var writer = new WaveFileWriter(wavFilePath, new WaveFormat((int)sample.SampleRate, (int)sample.SFSampleLink)))
+                        {
+                            //writer.Write(audioData, 0, audioData.Length);
+                        }
+                    }
+                }
+            }
         }
     }
 }
