dd7e99ded9a47a5bb165a00115d2224b319bdbb0
Gabriel Alabi <Rexes_Games@yahoo.com>
2023-07-01 16:51:09 +0100

    trying to preserve audio quality
    trying to preserve audio quality using audioclips

--- Unity Soundfont Player/Assets/SoundFont Player/Scripts/SoundFontPlayer.cs
+++ Unity Soundfont Player/Assets/SoundFont Player/Scripts/SoundFontPlayer.cs
@@ -7,9 +7,12 @@ using System.Threading;
 
 public class SoundFontPlayer : MonoBehaviour
 {
-    private SoundFont soundfont;
     [SerializeField] private string soundfontPath;
+    [SerializeField] private AudioSource audioSource;
+
+    private SoundFont soundfont;
     private MidiOut midiOut;
+    private AudioClip audioClip;
 
     public SoundFont Soundfont => soundfont;
 
@@ -66,16 +69,10 @@ public class SoundFontPlayer : MonoBehaviour
         }
 
         //Stop Playing existing Instrument first
-        StopInstrument();
+        //StopInstrument();
 
         // Create a new MIDI output device
-        midiOut = new MidiOut(0);
-
-        // Select the desired instrument using the MIDI program change message
-        // we can use the value 192 directly to represent the program change status byte.
-        //int programChangeStatus = 192 + instrumentIndex;
-        //var programChangeMessage = new MidiMessage(programChangeStatus, 0, 0);
-        //midiOut.Send(programChangeMessage.RawData);
+        //midiOut = new MidiOut(0);
 
         var instrument = soundfont.Instruments[instrumentIndex];
 
@@ -87,17 +84,39 @@ public class SoundFontPlayer : MonoBehaviour
                 if (generator.GeneratorType == GeneratorEnum.SampleID)
                 {
                     var sampleIndex = generator.UInt16Amount;
+                    var sample = soundfont.SampleHeaders[sampleIndex];
 
-                    // Play a note using the MIDI note on message
-                    var noteOnMessage = new MidiMessage((int)MidiCommandCode.NoteOn, sampleIndex, 100);
-                    midiOut.Send(noteOnMessage.RawData);
+                    // Calculate the note frequency based on the sample's original pitch and pitch correction
+                    var frequency = CalculateNoteFrequency(sample.OriginalPitch, sample.PitchCorrection);
 
-                    // Wait for a short duration to allow the sample to play
-                    Thread.Sleep(500); // Adjust the duration as needed
+                    // Set the desired buffer size and sample rate for the audio output
+                    int sampleRate = (int)sample.SampleRate;
 
-                    // Send the corresponding note off message to stop the sample
-                    var noteOffMessage = new MidiMessage((int)MidiCommandCode.NoteOff, sampleIndex, 0);
-                    midiOut.Send(noteOffMessage.RawData);
+                    // Create an AudioClip to hold the sample data
+                    audioClip = AudioClip.Create(sample.SampleName, (int)(sample.End - sample.Start), (int)sample.SFSampleLink, sampleRate, false);
+
+                    // Copy the sample data to the AudioClip
+                    byte sampleData = soundfont.SampleData[sampleIndex];
+
+                    // Convert byte to float and Normalize byte value to float range (0 to 1)
+                    float floatData = sampleData / 255f; 
+
+                    audioClip.SetData(new float[] { floatData }, 0);
+
+                    // Create a new AudioSource and play the AudioClip
+                    audioSource.clip = audioClip;
+                    audioSource.pitch = 3;
+                    audioSource.Play();
+                    return;
+
+
+                    // Wait for the sample to finish playing
+                    //Thread.Sleep((int)((sample.End - sample.Start) * 1000 / sampleRate));
+
+                    //Destroy the AudioSource and AudioClip
+                    //audioSource.Stop();
+                    //audioSource.clip = null;
+                    //audioClip = null;
                 }
             }
         }
@@ -117,5 +136,27 @@ public class SoundFontPlayer : MonoBehaviour
             midiOut.Dispose();
             midiOut = null;
         }
+    }
+
+    public float CalculateNoteFrequency(in int originalPitch, in int pitchCorrection)
+    {
+        // A4 is MIDI note number 69 with a frequency of 440 Hz
+        const float a4Frequency = 440f;
+        const int a4MidiNote = 69;
+
+        // Calculate the semitone difference from A4
+        int semitoneDifference = originalPitch - a4MidiNote;
+
+        //there are 12 semitones in an octave
+        const float octaveIncrement = 12f;
+
+        // Calculate the pitch factor based on the semitone difference and pitch correction
+        //Pitch ratio is 2^(n/12)
+        float pitchFactor = Mathf.Pow(2f, (semitoneDifference + pitchCorrection) / octaveIncrement);
+
+        // Calculate the note frequency
+        float noteFrequency = a4Frequency * pitchFactor;
+
+        return noteFrequency;
     }
 }
